60e68c357bc0dd15ddbc1f7ce3004e61ab81e216
Afshin Mortazavi-Nia <davidnia1@gmail.com>
2018-04-08 21:34:56 -0700

    .

---
+++ border/example/transparent/example-border-transparent.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"runtime"
+	"fmt"
+	"github.com/amortaza/go-bellina"
+	"github.com/amortaza/go-hal-g5"
+	"github.com/amortaza/go-dark-ux"
+	"github.com/amortaza/go-dark-ux/border"
+	"github.com/amortaza/go-dark-ux/label"
+)
+
+func initialize() {
+	go_dark_ux.Init()
+}
+
+func tick() {
+
+	bl.Root()
+	{
+		bl.Pos(0,0)
+		bl.Dim(1024, 768)
+
+		bl.Div()
+		{
+			bl.Id("red")
+			bl.Pos(10,10)
+			bl.Dim(800,600)
+
+			bl.Div()
+			{
+				bl.Id("green")
+				bl.Pos(100,100)
+				bl.Dim(400,300)
+
+				label.Id("ace").Label("Hello").End()
+
+				border.Wire(255, 255, 255)
+			}
+			bl.End()
+
+			border.Fill(100, 20, 100)
+		}
+		bl.End()
+	}
+	bl.End()
+}
+
+func uninit() {
+	go_dark_ux.Uninit()
+}
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func main() {
+	bl.Start( hal_g5.NewHal(), "Dark UX", 2000, 50, 1024, 768, initialize, tick, uninit )
+
+	fmt.Println("bye!")
+}
+
+
